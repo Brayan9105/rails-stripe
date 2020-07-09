0ae80fdbd9f70239c0d5aa25d9af4a3d56b716c8
Brayan Lopez <brayan.lopez@koombea.com>
2020-07-08 22:16:29 -0500

    Added route to added credit card + js stripe form file

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -3,4 +3,10 @@ class BillingController < ApplicationController
 
   def index
   end
+
+  def new_card
+    respond_to do |format|
+      format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'billing#index', as: :billing
+  
+  get '/card/new', to: 'billing#new_card', as: :add_payment_method
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
