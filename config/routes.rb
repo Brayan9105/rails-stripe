361caa50e8da37318c0952d5c2c164c797f2c1b2
Brayan Lopez <brayan.lopez@koombea.com>
2020-07-08 22:22:25 -0500

    Added create credit card method + success view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'billing#index', as: :billing
   
   get '/card/new', to: 'billing#new_card', as: :add_payment_method
+  post "/card", to: "billing#create_card", as: :create_payment_method
+
+  get '/success', to: 'billing#success', as: :success
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
